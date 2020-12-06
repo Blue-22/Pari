ad26dcd32195c61ed956c55cdd0af2e645a0cc84
Jérémy Cartier <jerry.c-m@live.fr>
2020-12-06 17:48:28 +0100

    Init accueil

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Laravel</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+<body class="antialiased">
+
+  <header class="header">
+    <h1>Paris Sportif</h1>
+  </header>
+  <div class="current-bet-container container">
+    <div class="cards-list">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Nom Rencontre</h5>
+          <p class="card-text">Description de la rencontre.</p>
+          <a href="#" class="card-link">> Détails <</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Nom Rencontre</h5>
+          <p class="card-text">Description de la rencontre.</p>
+          <a href="#" class="card-link">> Détails <</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Nom Rencontre</h5>
+          <p class="card-text">Description de la rencontre.</p>
+          <a href="#" class="card-link">> Détails <</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Nom Rencontre</h5>
+          <p class="card-text">Description de la rencontre.</p>
+          <a href="#" class="card-link">> Détails <</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="bet-list container">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Nom Rencontre</h5>
+        <p class="card-text">Description de la rencontre.</p>
+        <a href="#" class="card-link">Détails</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Nom Rencontre</h5>
+        <p class="card-text">Description de la rencontre.</p>
+        <a href="#" class="card-link">Détails</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Nom Rencontre</h5>
+        <p class="card-text">Description de la rencontre.</p>
+        <a href="#" class="card-link">Détails</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Nom Rencontre</h5>
+        <p class="card-text">Description de la rencontre.</p>
+        <a href="#" class="card-link">Détails</a>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
